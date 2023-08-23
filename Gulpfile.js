f501fc0e674065be2f396b2c25a36381f3d5b9ed
Volodymyr Honcharuk <volodymyrh94elogic@gmail.com>
2023-08-23 11:40:43 +0300

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,78 @@
+// Gulp
+const gulp = require('gulp');
+
+// Runtime Utilities
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const shell = require('gulp-shell');
+
+// CSS/SCSS
+const sass = require('gulp-sass')(require('node-sass'));
+const cssmin = require('gulp-clean-css');
+
+// JS
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+// Add store handle here
+let shopify_url = ''
+
+// Keep customizer in sync when running the CLI? Assign '--theme-editor-sync' key to additionalCLIArgs variable.
+let additionalCLIArgs = '';
+
+// Error notifier when Gulp is running
+let plumberErrorHandler = {
+  errorHandler: notify.onError({
+    title: '<%= error.plugin %>',
+    message: 'Error: <%= error.message %>',
+  }),
+};
+
+// CSS task
+gulp.task('styles', () => {
+  return gulp
+    .src(['src/styles/*.scss'])
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(gulp.src('src/styles/*.css'))
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(cssmin())
+    .pipe(gulp.dest('assets/'));
+});
+
+// Javascript task
+gulp.task('scripts', () => {
+  const extJs = ['./node_modules/swiper/swiper-bundle.js']; // Add npm js assets here
+  return gulp
+    .src(['src/scripts/*.js'].concat(extJs))
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/'));
+});
+
+// Compile CSS & JS, send updates to /deploy folder
+gulp.task('build', gulp.series('styles', 'scripts'));
+
+// Run Shopify CLI; prompts for login as necessary
+gulp.task(
+  'shopify-cli',
+  shell.task([
+    `shopify theme dev --store ${shopify_url} ${additionalCLIArgs}`,
+  ])
+);
+
+// Watch all local changes in /src
+gulp.task(
+  'watch',
+  gulp.series('build', async function watching() {
+    gulp.watch(
+      ['src/styles/**/*.scss', 'src/styles/**/*.css'],
+      gulp.series('styles')
+    );
+    gulp.watch(['src/scripts/**/*.js'], gulp.series('scripts'));
+  })
+);
+
+// Execute this task queue on "gulp" command
+gulp.task('default', gulp.series('watch', 'shopify-cli'));
